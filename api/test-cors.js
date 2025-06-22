00fe74a8e9437f39e06fca6c3511ae1598acaefd
Darlington Odom <darlingtonodom@gmail.com>
2025-06-22 19:26:40 +0100

    Add simple CORS test endpoint

---
+++ api/test-cors.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  res.status(200).json({ 
+    message: 'CORS test successful',
+    method: req.method,
+    origin: req.headers.origin 
+  });
+}
